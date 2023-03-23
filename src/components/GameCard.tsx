c5c255a560ba6ab8df0aafe5abe353a7cea1f3df
Andreas Vrachimi <avrachimi@hotmail.com>
2023-03-23 21:21:14 +0000

    Cleanup GameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,14 +10,16 @@ interface Props {
 
 function GameCard({ game }: Props) {
 	return (
-		<Card height={{ lg: 330 }}>
+		<Card>
 			<Image src={getCroppedImageURL(game.background_image)} />
 			<CardBody>
-				<Heading fontSize="2xl">{game.name}</Heading>
-				<HStack justify={"space-between"}>
+				<HStack justify={"space-between"} marginBottom={3}>
 					<PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
 					<CriticScore score={game.metacritic} />
 				</HStack>
+				<Heading fontSize={{ sm: "2xl", md: "2xl", lg: "sm", xl: "md" }}>
+					{game.name}
+				</Heading>
 			</CardBody>
 		</Card>
 	);
