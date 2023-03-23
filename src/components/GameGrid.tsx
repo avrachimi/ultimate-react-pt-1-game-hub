22037c0cb803a88e5d97b2df0c7edb9bd59b458f
Andreas Vrachimi <avrachimi@hotmail.com>
2023-03-23 17:37:14 +0000

    Fetch game genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,7 +6,7 @@ import GameCardSkeleton from "./GameCardSkeleton";
 
 function GameGrid() {
 	const { games, error, isLoading } = useGames();
-	const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 	return (
 		<>
